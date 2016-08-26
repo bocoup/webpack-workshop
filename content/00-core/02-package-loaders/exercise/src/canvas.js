2b94209dad6d2a36cd6b37af220c70e1ac1e38a5
Corey Frang <gnarf@gnarf.net>
2016-08-26 12:07:06 -0400

    Remove fabric from first excercise

--- content/00-core/02-package-loaders/exercise/src/canvas.js
+++ content/00-core/02-package-loaders/exercise/src/canvas.js
@@ -1,48 +1,42 @@
-var canvas = new fabric.Canvas('canvas');
+var canvas = document.querySelector('#canvas');
+var context = canvas.getContext('2d');
 
-var subtitle = new fabric.Text('Hello, world', {
-  fontFamily: '"Helvetica Neue", helvetica',
-  fontSize: 24,
-  fill: '#fff',
-  top: 520,
-  selectable: false
-});
-
-var border = new fabric.Rect({
-  color: '#000',
-  width: 716,
-  height: 596,
-  left: 2,
-  top: 2,
-  selectable: false
-});
-
-function addBorder() {
-  canvas.add(border);
+var backgroundStyles = {
+  fillStyle: '#000',
 }
 
-function addSubtitle() {
-  canvas.add(subtitle);
-  subtitle.centerH();
+var subtitleStyles = {
+  font: '24px "Helvetica Neue", helvetica',
+  fillStyle: '#fff',
+  textAlign: 'center',
+  textBaseline: 'top',
+};
+
+var canvasImageCopy = new Image();
+canvasImageCopy.onload = function() {
+  context.drawImage(canvasImageCopy, 60, 60, 600, 450);
+};
+
+function drawMeme(image, text) {
+  var width = canvas.getAttribute('width');
+  var height = canvas.getAttribute('height');
+  var borderPadding = 2;
+
+  context.fillStyle = '#fff';
+  context.fillRect(0, 0, width, height);
+
+  context.fillStyle = '#000';
+  context.fillRect(borderPadding, borderPadding, width-borderPadding*2, height-borderPadding*2);
+
+  for (key in subtitleStyles) {
+    context[key] = subtitleStyles[key];
+  }
+  var measured = context.measureText(text);
+  context.fillText(text, width / 2, height - 80);
+
+  if (canvasImageCopy.src === image.src) {
+    context.drawImage(canvasImageCopy, 60, 60, 600, 450);
+  } else {
+    canvasImageCopy.src = image.src;
+  }
 }
-
-function updateSubtitle(value) {
-  subtitle.setText(value);
-  subtitle.centerH();
-} 
-
-var imgInstance;
-
-function replaceImage(img) {
-  canvas.remove(imgInstance);
-
-  imgInstance = new fabric.Image(img, {
-    left: 60,
-    top: 60,
-    width: 600,
-    height: 450,
-    selectable: false
-  });
-  canvas.add(imgInstance);
-}
-
