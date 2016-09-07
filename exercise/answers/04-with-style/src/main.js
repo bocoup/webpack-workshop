5cc5838c175a81a7273f4e8da3ac63eab83a8b4d
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-09-07 17:06:45 -0400

    Add fourth exercise - style and file loading

---
+++ exercise/answers/04-with-style/src/main.js
@@ -0,0 +1,30 @@
+import './styles/all.css';
+
+import jQuery from '../vendor/jquery-3.1.0';
+
+import {drawMeme} from './canvas';
+import * as memes from './memes';
+
+jQuery($ => {
+  const memeKeys = Object.keys(memes);
+
+  $('.select-image img').each((index, img) => {
+    $(img).attr('src', memes[memeKeys[index]]);
+  });
+
+  let currentImage = $('.image-selector img')[0];
+  let currentText = "Hello, world!";
+
+  drawMeme(currentImage, currentText);
+
+  $('.select-image').click(ev => {
+    ev.preventDefault();
+    currentImage = $('img', ev.currentTarget)[0];
+    drawMeme(currentImage, currentText);
+  });
+
+  $('.edit-subtitle').on('keyup input', ev => {
+    currentText = ev.target.value;
+    drawMeme(currentImage, currentText);
+  });
+});
