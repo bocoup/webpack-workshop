f96bc826a98497d62ba7561401887d825339a7dc
Corey Frang <gnarf@gnarf.net>
2016-09-12 13:17:01 -0400

    Rework base example

--- exercise/src/main.js
+++ exercise/src/main.js
@@ -1,17 +1,34 @@
 jQuery(function($) {
-  var currentImage = $('.image-selector img')[0];
-  var currentText = "Hello, world!";
+  var imageUrls = [
+    'img/bunny-725x544.jpg',
+    'img/funny-monkey-725x544.jpg',
+    'img/guinea-pigs-725x544.jpg',
+  ];
 
+  // Generate Image List
+  var imageSelector = $('.image-selector');
+  imageUrls.forEach(function(url) {
+    $('<li><a href="#" class="select-image">' +
+      '<img src="' + url + '" width="120" height="90"></a></li>')
+      .appendTo(imageSelector);
+  })
+
+  // Pick the first image by default
+  var currentImage = $('.image-selector img')[0];
+  var currentText = 'Hello, world!';
   drawMeme(currentImage, currentText);
 
-  $('.select-image').click(function(ev) {
-    ev.preventDefault();
-    currentImage = $('img', this)[0];
+  // Listen for clicks on images
+  imageSelector.on('click', '.select-image', function(event) {
+    event.preventDefault();
+    currentImage = $('img', event.target);
     drawMeme(currentImage, currentText);
   });
 
-  $('.edit-subtitle').on('keyup input', function(ev) {
-    currentText = ev.target.value;
+  // Listen for input and key events for the text box
+  $('.edit-subtitle').on('keyup input', function(event) {
+    currentText = event.target.value;
     drawMeme(currentImage, currentText);
   });
+
 });
