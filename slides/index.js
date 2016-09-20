c74fd2ada3b6da90dd8800ff5047c9cb1be1b11c
Corey Frang <gnarf@gnarf.net>
2016-09-20 15:06:03 -0400

    only require slides in "debug mode"

--- slides/index.js
+++ slides/index.js
@@ -1,18 +1,26 @@
 var remark = require('remark');
 var slideshow = remark.create();
 
-var slides = require('./slide-source');
-// Ensure the rendered slides match the source in this script.
-slideshow.loadFromString(slides);
-if (module.hot) {
-  // Hot replace any changes.
-  module.hot.accept('./slide-source', function() {
-    var slides = require('./slide-source');
-    slideshow.loadFromString(slides);
-    var thisSlide = slideshow.getCurrentSlideIndex() + 1;
 
-    // Go to first slide, unless first slide, then go to second
-    slideshow.gotoSlide(thisSlide === 1 ? 2 : 1);
-    slideshow.gotoSlide(thisSlide);
-  });
+// Ensure the rendered slides match the source in this script.
+slideshow.loadFromString(document.getElementById('source').value);
+
+// in debug mode...
+if (process.env.NODE_ENV !== 'production') {
+  var slides = require('./slide-source');
+
+  // HMR addition only
+  if (module.hot) {
+    // Hot replace any changes.
+    module.hot.accept('./slide-source', function() {
+      var slides = require('./slide-source');
+      slideshow.loadFromString(slides);
+      var thisSlide = slideshow.getCurrentSlideIndex() + 1;
+
+      // Go to first slide, unless first slide, then go to second
+      slideshow.gotoSlide(thisSlide === 1 ? 2 : 1);
+      slideshow.gotoSlide(thisSlide);
+    });
+  }
+
 }
