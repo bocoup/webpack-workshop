b178395a7e064f1260fa99d583d73705cb4aec52
Corey Frang <gnarf37@gmail.com>
2017-03-09 11:00:58 -0500

    Excercise 1 answer

--- exercise/answers/04-with-style/src/memes/index.js
+++
@@ -1,9 +0,0 @@
-import bunny from './bunny-725x544.jpg';
-import monkey from './funny-monkey-725x544.jpg';
-import guineaPigs from './guinea-pigs-725x544.jpg';
-
-export {
-  bunny,
-  monkey,
-  guineaPigs,
-};
